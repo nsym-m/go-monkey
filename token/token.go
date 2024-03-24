aff402474e31c255c07a4ccf93650a69425476b9
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-03-24 21:27:10 +0900

    add const

--- token/token.go
+++ token/token.go
@@ -1,5 +1,31 @@
 package token
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foober, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
 type TokenType string
 
 type Token struct {
